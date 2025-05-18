d29ea2751c9aca0a931401c2c80d2c024056cca0
stephanie-caroline <stephanie.moreira1994@gmail.com>
2025-05-18 19:41:41 -0300

    add projeto

---
+++ frontend/js/script.js
@@ -0,0 +1,22 @@
+async function loadNews() {
+  try {
+    const response = await fetch('../../data/noticias.json');
+    const data = await response.json();
+
+    const container = document.getElementById('noticias');
+
+    data.forEach((noticia) => {
+      const titulo = document.createElement('h2');
+      titulo.classList.add('news');
+      titulo.textContent = noticia.titulo;
+
+      container.appendChild(titulo);
+    });
+  } catch (err) {
+    document.getElementById('noticias').textContent =
+      'Erro ao carregar notícias.';
+    console.error(err);
+  }
+}
+
+loadNews();
---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def get_news():
+  url = "https://www.uol.com.br/"
+  response = requests.get(url)
+
+  noticias = []
+  
+  soup = BeautifulSoup(response.text, "html.parser")
+  tags = soup.find_all('h3')[:10]
+
+  for tag in tags:
+    titulo = tag.text.strip()
+    noticias.append({ "titulo": titulo })
+  return noticias
+
+def save_json(noticias):
+  with open("data/noticias.json", "w", encoding="utf-8") as arquivo:
+    json.dump(noticias, arquivo)
+
+noticias = get_news()
+save_json(noticias)
+print("Arquivo noticias.json gerado com sucesso!")
